82e5dff5d384857fb5d4d8d9bcaf8a2809741744
Iswanul Umam <umam@sepulsa.com>
2018-06-29 01:28:23 +0700

    create action type reducer

--- src/store/action-type.js
+++ src/store/action-type.js
@@ -1,1 +1,12 @@
 export const EXAMPLE_ACTION = 'EXAMPLE_ACTION';
+// @ main
+export const USER_REGISTER = 'USER_REGISTER';
+export const USER_LOGIN = 'LOGIN';
+export const USER_LOGOUT = 'USER_LOGOUT';
+export const GET_MY_PROFILE = 'GET_MY_PROFILE';
+
+export const POST_TASK = 'POST_TASK';
+export const GET_ALL_TASK = 'GET_ALL_TASK';
+export const GET_ONE_TASK = 'GET_ONE_TASK';
+export const UPDATE_TASK = 'UPDATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
