7d512a4248f951ebb655b933a432da6825782d94
Iswanul Umam <umam@sepulsa.com>
2018-06-29 10:54:35 +0700

    get profile action finish

---
+++ src/store/task/action.js
@@ -0,0 +1,5 @@
+import { POST_TASK, GET_ALL_TASK, GET_ONE_TASK, UPDATE_TASK, DELETE_TASK } from './../action-type';
+import axios from 'axios';
+
+const baseUrl = `http://ec2-18-191-188-60.us-east-2.compute.amazonaws.com`;
+
--- src/store/user/action.js
+++ src/store/user/action.js
@@ -1,11 +1,19 @@
-import { USER_REGISTER, USER_LOGIN, USER_LOGOUT, GET_PROFILE } from './../action-type';
+import { GET_PROFILE } from './../action-type';
 import axios from 'axios';
 
-const baseUrl = `http://ec2-18-191-188-60.us-east-2.compute.amazonaws.com`;
-
-const userRegisterAction = () => {
+// @ get profile
+export const getProfileAction = (token) => {
   return dispatch => {
-    
+    axios.get(`http://ec2-18-191-188-60.us-east-2.compute.amazonaws.com/me`, { 'x-auth': token })
+      .then(response => {
+        dispatch(getProfile(response.data.user));
+      }).catch(e => {
+        console.log(e);
+      })
   }
 }
 
+const getProfile = (payload) => ({
+  type: GET_PROFILE,
+  payload: payload
+});
