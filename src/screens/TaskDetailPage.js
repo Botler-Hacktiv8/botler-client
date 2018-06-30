af8c7915fa81bd3d01377c9eadd2d7a273148dcf
Jovian Andrew <jovian_andrew@hotmail.com>
2018-06-30 16:50:28 +0700

    added task detail page template

---
+++ src/screens/TaskDetailPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native-elements'
+
+class TaskDetailPage extends Component {
+  render() {
+    return (
+      <View>
+          
+      </View>
+    );
+  }
+}
+
+export default TaskDetailPage;
