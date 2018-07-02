455b5e7bbf82bb931476a1a2c21caa395d181bbf
Iswanul Umam <umam@sepulsa.com>
2018-07-02 19:40:24 +0700

    add token to userData

--- src/store/user/action.js
+++ src/store/user/action.js
@@ -7,7 +7,11 @@ export const getProfileAction = (token) => {
     console.log('getProfileAction token here', token);
     axios.get(`http://ec2-18-191-188-60.us-east-2.compute.amazonaws.com/api/me`, { headers: { 'x-auth': token } })
       .then(response => {
-        dispatch(getProfile(response.data.user));
+        const userData = {
+          ...response.data.user,
+          token: token,
+        }
+        dispatch(getProfile(userData));
       }).catch(e => {
         console.log(e);
       });
