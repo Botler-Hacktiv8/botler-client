7f9f664fba316edebeb68efddb49aacd8a64d692
Iswanul Umam <umam@sepulsa.com>
2018-07-03 13:37:41 +0700

    fix logout

--- src/screens/Logout.js
+++ src/screens/Logout.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { AsyncStorage, View } from 'react-native';
-import axios from 'axios'
+import axios from 'axios';
+
+// @ redux config
+import { connect } from 'react-redux';
 
 class Logout extends Component {
   componentWillMount() {
@@ -10,7 +13,7 @@ class Logout extends Component {
   // @ remove token and move to login page
   logout = () => {
     let self = this
-    axios.delete(`http://ec2-18-191-188-60.us-east-2.compute.amazonaws.com/api/logout`, { headers: { 'x-auth': this.state._UserToken } })
+    axios.delete(`http://ec2-18-191-188-60.us-east-2.compute.amazonaws.com/api/logout`, { headers: { 'x-auth': this.props.token } })
     .then(() => {
       self._removeToken();
       this.props.screenProps.logout();
@@ -33,10 +36,13 @@ class Logout extends Component {
   render() {
     return (
       <View>
-            
       </View>
     );
   }
 }
 
-export default Logout;
+const mapStateToProps = (state) => ({
+  token: state.userState.userData.token
+});
+
+export default connect(mapStateToProps, null)(Logout);
